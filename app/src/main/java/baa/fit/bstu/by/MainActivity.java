59d0e3df150b4bf1c2c545bbe04363170e8babc9
Artyom Belski <artbelski@gmail.com>
2021-09-07 08:32:57 +0300

    Last

--- app/src/main/java/baa/fit/bstu/by/MainActivity.java
+++ app/src/main/java/baa/fit/bstu/by/MainActivity.java
@@ -3,6 +3,10 @@ package baa.fit.bstu.by;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import baa.fit.bstu.by.text.TextFunctionRenamed;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", " counter =" + count);
+        }
+        
+        displayText();
+    }
+
+    private void displayText() {
+        TextFunctionRenamed tfr = new TextFunctionRenamed();
+        TextView nt = findViewById(R.id.tw_id);
+        nt.setText(tfr.getValue());
     }
 }
---
+++ app/src/main/java/baa/fit/bstu/by/text/TextFunctionRenamed.java
@@ -0,0 +1,8 @@
+package baa.fit.bstu.by.text;
+
+public class TextFunctionRenamed {
+    public String getValue(){
+        return "Hello from first project. I'm Artyom Belski";
+    }
+}
+
